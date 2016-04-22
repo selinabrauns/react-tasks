f30e2dac865c3a8cab513dd3bbd7e590a41ab166
selinabrauns <s.brauns@leverton.de>
2016-04-22 17:46:10 +0200

    added cols to html

--- react/components/elements/page.jsx
+++ react/components/elements/page.jsx
@@ -5,7 +5,8 @@ module.exports = React.createClass({
         return <div className='main'>
             <div className='header'><img src='img/lev_logo_rgb_web.png'/></div>
             <div className='page-content'>
-
+                <div className='col'></div>
+                <div className='col'></div>
             </div>
             <div className='footer'><p>Good luck!</p></div>
         </div>
